eea51b4299dfc71f5aa76d5cddabf2745a88c9b1
Angel Oyarzun <deagleeanzun@gmail.com>
2020-04-07 20:02:18 -0400

    mensajes de error en plantilla base. Antes era en cada vista.

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,21 +1,6 @@
 @extends('layouts.dash')
 
 @section('content')
-
-@if(count($errors) > 0)
-	<div class="alert alert-danger alert-dismissible fade show" role="alert">
-		<ul>
-			@foreach($errors->all() as $error)
-			<li>{{ $error }}</li>
-			@endforeach
-		</ul>
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-		    <span aria-hidden="true">&times;</span>
-		</button>
-	</div>
-
-@endif
-
 <div class="card">
 	<div class="card-header">
 		<div class="row">
